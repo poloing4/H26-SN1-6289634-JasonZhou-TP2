7f751193e68ffe58660354fc23a19f938ce65a89
poloi <poloing4@outlook.com>
2026-04-29 00:19:00 -0400

    gomit

--- analyse.py
+++ analyse.py
@@ -13,3 +13,12 @@ def QA3NombreFeuilluConifere():
     return (feuillu,conifère)
 def QA4ArbreAimemivile():
     df=pd.read_csv("vdq-arbrerepertorie.csv")
+    df=df.dropna(subset=["NOM_TOPOGRAPHIE", "NOM_FRANCAIS"])
+    parc=df[df['NOM_TOPOGRAPHIE'].str.contains('Parc Aimée-Miville')]
+    especes=parc["NOM_FRANCAIS"].drop_duplicates().sort_values()
+    return especes.to_list()
+def QA5Especegros():
+    df=pd.read_csv("vdq-arbrerepertorie.csv")
+    grostronc=df.sort_values(by='DIAMETRE', ascending=False)
+    return (grostronc.iloc[0]['NOM_LATIN'])
+print(QA5Especegros())
